7c823f5e17a2b1990a42504d83fde11eefa378c5
Adil-Arshad <adilarshad3347@gmail.com>
2020-03-18 22:45:50 +0500

    update

--- src/app/geolocation/geolocation.page.ts
+++ src/app/geolocation/geolocation.page.ts
@@ -1,5 +1,5 @@
 
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild,NgZone, Input, OnDestroy} from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild, NgZone, Input, OnDestroy } from '@angular/core';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { LocationStrategy } from '@angular/common';
 import { UserService } from '../sdk/custom/user.service';
@@ -7,7 +7,7 @@ import { analyzeFile } from '@angular/compiler';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
-declare var google:any;
+declare var google: any;
 @Component({
   selector: 'app-geolocation',
   templateUrl: './geolocation.page.html',
@@ -18,7 +18,7 @@ export class GeolocationPage implements AfterViewInit, OnInit {
   latitude: number;
   longitude: number;
   geoMarker: any;
-  autocomplete={input:''};
+  autocomplete = { input: '' };
   autocompleteItems = [];
   GoogleAutocomplete = new google.maps.places.AutocompleteService();
   geocoder = new google.maps.Geocoder;
@@ -31,162 +31,145 @@ export class GeolocationPage implements AfterViewInit, OnInit {
   foundproductsdata; //getting found products
   foundpersonsdata; //getting found persons
 
-  searchTerm='';
-  results:Observable<any>;
- 
-  myLatLng; 
+  searchTerm = '';
+  results: Observable<any>;
+  Searchposition:any;
+  myLatLng;
   address: any;
-  
-  constructor(private router:Router,private geolocation: Geolocation,private zone:NgZone, private userService: UserService) { }
-    map: google.maps.Map;
-    lat = 30.3760;
-    lng = 69.3451;
 
-    
-    datacollector(data){
-      this.myLatLng = data;
-      for(var i = 0; i<data.length; i++){
-        this.myLatLng[i].lat=data[i].lat;
-        this.myLatLng[i].lng=data[i].lng;
-      }
+  constructor(private router: Router, private geolocation: Geolocation, private zone: NgZone, private userService: UserService) { }
+  map: google.maps.Map;
+  lat = 30.3760;
+  lng = 69.3451;
+
+
+  datacollector(data) {
+    this.myLatLng = data;
+    for (var i = 0; i < data.length; i++) {
+      this.myLatLng[i].lat = data[i].lat;
+      this.myLatLng[i].lng = data[i].lng;
     }
- 
-
-    coordinates = new google.maps.LatLng(this.lat, this.lng);
-    
-// page mapoptions
-    mapOptions: google.maps.MapOptions = {
-     center: this.coordinates,
-     disableDefaultUI: true,
-     zoom: 7
-     //mapTypeId: 'satellite'
-    };
-//map options for geolocation
-    mapOptions1: google.maps.MapOptions = {
-      center: this.coordinates,
-      disableDefaultUI: true,
-      zoom: 18
-     };
-
-
-ngOnInit(){
-  // this.sub = this.route
-  // .queryParams
-  // .subscribe(params => {
-  //   // Defaults to 0 if no query param provided.
-  //   this.queryParameters = +params['page'] || 0;
-  //   this.uniqueID=params.uniqueid;
-  //   this.latt = params.laatitude;
-  //   this.lngg = params.loongitude;
-  //   console.log('ID:',this.uniqueID);
-  //   console.log('latt:',this.latt);
-  //   console.log('lngg:',this.lngg);
-  // }); 
-
-  //getting data from lost products 
-  this.userService.getAllLostProducts().subscribe(
-    alllostproducts => {
-      console.log("1st record  products", alllostproducts);
-      this.lostproductsdata = alllostproducts;
-      console.log("all lost products", this.lostproductsdata[0].lat);
-      
-    },
-    err => {
-      console.log("api error in all request retrieval", err);
-    }
-  ); 
-
-  //getting data from lost persons 
-  this.userService.getAllLostPersons().subscribe(
-    alllostpersons => {
-      console.log("1st record  products", alllostpersons);
-      this.lostpersonsdata = alllostpersons;
-      //console.log("all lost products", this.lostproductsdata[0].lat);
-    },
-    err => {
-      console.log("api error in data retrieval", err);
-    }
-  ); 
-
-  
-  //getting data from found products 
-  this.userService.getAllFoundProducts().subscribe(
-    allfoundproducts => {
-      this.foundproductsdata = allfoundproducts;
-     // console.log("all found products", this.foundproductsdata[0].lat);
-      
-    },
-    err => {
-      console.log("api error in all request retrieval", err);
-    }
-  ); 
-
-  //getting data from found persons 
-  this.userService.getAllFoundPersons().subscribe(
-    allfoundpersons => {
-      this.foundpersonsdata = allfoundpersons;
-      //console.log("all lost products", this.lostproductsdata[0].lat);
-    },
-    err => {
-      console.log("api error in data retrieval", err);
-    }
-  ); 
-  //this.displayallmarkers();
-}
-////////////////////////////////////////Current search bar test
-/*
-ionViewLoaded() {
-  let elem = <HTMLInputElement>document.getElementsByClassName('searchbar-input')[0];
-  this.autocomplete = new google.maps.places.Autocomplete(elem);
-}
-
-getAddress(place: Object) {       
-  this.address = place['formatted_address'];
-  var location = place['geometry']['location'];
-  var lat =  location.lat();
-  var lng = location.lng();
-  console.log('Address Object', place);
-}*/
-///////////////////////////////////////////////////////////////
-
-    ngAfterViewInit() {
-      this.mapInitializer();
-      
   }
 
-    mapInitializer() {
-      this.map = new google.maps.Map(this.gmap.nativeElement, 
+
+  coordinates = new google.maps.LatLng(this.lat, this.lng);
+
+                                             // page mapoptions
+  mapOptions: google.maps.MapOptions = {
+    center: this.coordinates,
+    disableDefaultUI: true,
+    zoom: 7
+    //mapTypeId: 'satellite'
+  };
+                                            //map options for geolocation
+  mapOptions1: google.maps.MapOptions = {
+    center: this.coordinates,
+    disableDefaultUI: true,
+    zoom: 18
+  };
+
+
+  ngOnInit() {
+    // this.sub = this.route
+    // .queryParams
+    // .subscribe(params => {
+    //   // Defaults to 0 if no query param provided.
+    //   this.queryParameters = +params['page'] || 0;
+    //   this.uniqueID=params.uniqueid;
+    //   this.latt = params.laatitude;
+    //   this.lngg = params.loongitude;
+    //   console.log('ID:',this.uniqueID);
+    //   console.log('latt:',this.latt);
+    //   console.log('lngg:',this.lngg);
+    // }); 
+
+    //getting data from lost products 
+    this.userService.getAllLostProducts().subscribe(
+      alllostproducts => {
+        console.log("1st record  products", alllostproducts);
+        this.lostproductsdata = alllostproducts;
+        console.log("all lost products", this.lostproductsdata[0].lat);
+
+      },
+      err => {
+        console.log("api error in all request retrieval", err);
+      }
+    );
+
+                                                    //getting data from lost persons 
+
+    this.userService.getAllLostPersons().subscribe(
+      alllostpersons => {
+        console.log("1st record  products", alllostpersons);
+        this.lostpersonsdata = alllostpersons;
+        //console.log("all lost products", this.lostproductsdata[0].lat);
+      },
+      err => {
+        console.log("api error in data retrieval", err);
+      }
+    );
+
+
+                                                    //getting data from found products 
+
+    this.userService.getAllFoundProducts().subscribe(
+      allfoundproducts => {
+        this.foundproductsdata = allfoundproducts;
+        // console.log("all found products", this.foundproductsdata[0].lat);
+
+      },
+      err => {
+        console.log("api error in all request retrieval", err);
+      }
+    );
+
+    //getting data from found persons 
+    this.userService.getAllFoundPersons().subscribe(
+      allfoundpersons => {
+        this.foundpersonsdata = allfoundpersons;
+        //console.log("all lost products", this.lostproductsdata[0].lat);
+      },
+      err => {
+        console.log("api error in data retrieval", err);
+      }
+    );
+    //this.displayallmarkers();
+  }
+  
+
+  ngAfterViewInit() {
+    this.mapInitializer();
+
+  }
+
+  mapInitializer() {
+    this.map = new google.maps.Map(this.gmap.nativeElement,
       this.mapOptions);
+  }
 
-     //Runtime add marker
-      // this.map.addListener('click', (event) => {
-      //   this.addMarker(event.latLng);
-      //   });
-    }
-    
-   
 
-    addInfoWindow(marker, content: string) {
-      let infoWindow = new google.maps.InfoWindow({
-        content: content
-      });
-      google.maps.event.addListener(marker, 'click',  () => {
-        infoWindow.open(this.map, marker);
-        console.log('clicked marker, markerType value:',marker.get('markerType'));
-       //this.router.navigate(['/description-page']);
-       if((marker.get('markerType') == 'lostperson')||(marker.get('markerType') == 'lostproduct')){
-       this.router.navigate(['/lost'], { queryParams: { markerID: marker.get('store_id'),markertype: marker.get('markerType') }});
-      console.log('inside lost url: markertype value',marker.get('markerType')); 
+
+  addInfoWindow(marker, content: string) {
+    let infoWindow = new google.maps.InfoWindow({
+      content: content
+    });
+    google.maps.event.addListener(marker, 'click', () => {
+      infoWindow.open(this.map, marker);
+      console.log('clicked marker, markerType value:', marker.get('markerType'));
+      //this.router.navigate(['/description-page']);
+      if ((marker.get('markerType') == 'lostperson') || (marker.get('markerType') == 'lostproduct')) {
+        this.router.navigate(['/lost'], { queryParams: { markerID: marker.get('store_id'), markertype: marker.get('markerType') } });
+        console.log('inside lost url: markertype value', marker.get('markerType'));
       }
-       else if((marker.get('markerType') == 'foundperson')||(marker.get('markerType') == 'foundproduct')){
-        this.router.navigate(['/found'], { queryParams: { markerID: marker.get('store_id'),markertype: marker.get('markerType') } });
-        console.log('inside found url: markertype value',marker.get('markerType'));  
+      else if ((marker.get('markerType') == 'foundperson') || (marker.get('markerType') == 'foundproduct')) {
+        this.router.navigate(['/found'], { queryParams: { markerID: marker.get('store_id'), markertype: marker.get('markerType') } });
+        console.log('inside found url: markertype value', marker.get('markerType'));
       }
-      })
-    }
-    
+    })
+  }
 
-    
+
+
 
   setMapOnAll(map) {
     for (var i = 0; i < this.markers.length; i++) {
@@ -200,7 +183,7 @@ getAddress(place: Object) {
 
 
   displayallmarkers() {
-    
+
     //lost product markers
     this.datacollector(this.lostproductsdata);
     const icon = {
@@ -209,14 +192,14 @@ getAddress(place: Object) {
       scaledSize: new google.maps.Size(50, 50), // scaled size
     };
     for (var i = 0; i < this.myLatLng.length; i++) {
-       this.marker = new google.maps.Marker({
+      this.marker = new google.maps.Marker({
         position: this.myLatLng[i],
         map: this.map,
         icon: icon,
         store_id: this.lostproductsdata[i]._id,
         markerType: 'lostproduct'
       });
-      console.log('this is markers id:',this.marker.get('store_id'));
+      console.log('this is markers id:', this.marker.get('store_id'));
       //console.log(this.lostproductsdata[i]._id);
       let content: string = 'Lost Product';
       this.addInfoWindow(this.marker, content);
@@ -229,19 +212,19 @@ getAddress(place: Object) {
       scaledSize: new google.maps.Size(50, 50), // scaled size
     };
     for (var i = 0; i < this.myLatLng.length; i++) {
-       this.marker = new google.maps.Marker({
+      this.marker = new google.maps.Marker({
         position: this.myLatLng[i],
         map: this.map,
         icon: icon1,
         store_id: this.lostpersonsdata[i]._id,
         markerType: 'lostperson',
       });
-      console.log('this is markers id:',this.marker.get('store_id'));
-      console.log('this is markers type:',this.marker.get('markerType'));
+      console.log('this is markers id:', this.marker.get('store_id'));
+      console.log('this is markers type:', this.marker.get('markerType'));
       let content: string = 'Lost Person';
-      this.addInfoWindow(this.marker, content);      
+      this.addInfoWindow(this.marker, content);
       //this.marker.setMap(this.map);
-     //this.markers.push(marker);
+      //this.markers.push(marker);
     }
 
     //found product markers
@@ -252,19 +235,19 @@ getAddress(place: Object) {
       scaledSize: new google.maps.Size(50, 50), // scaled size
     };
     for (var i = 0; i < this.myLatLng.length; i++) {
-       this.marker = new google.maps.Marker({
+      this.marker = new google.maps.Marker({
         position: this.myLatLng[i],
         map: this.map,
         icon: icon2,
         store_id: this.foundproductsdata[i]._id,
         markerType: 'foundproduct'
       });
-      console.log('this is markers id:',this.marker.get('store_id'));
+      console.log('this is markers id:', this.marker.get('store_id'));
       //console.log(this.lostproductsdata[i]._id);
       let content: string = 'Found Product';
       this.addInfoWindow(this.marker, content);
       //this.marker.setMap(this.map);
-     //this.markers.push(marker);
+      //this.markers.push(marker);
     }
     //lost person markers
     this.datacollector(this.foundpersonsdata);
@@ -274,116 +257,116 @@ getAddress(place: Object) {
       scaledSize: new google.maps.Size(50, 50), // scaled size
     };
     for (var i = 0; i < this.myLatLng.length; i++) {
-       this.marker = new google.maps.Marker({
+      this.marker = new google.maps.Marker({
         position: this.myLatLng[i],
         map: this.map,
         icon: icon3,
         store_id: this.foundpersonsdata[i]._id,
         markerType: 'foundperson'
       });
-      console.log('this is markers id:',this.marker.get('store_id'));
-      console.log('this is markers type:',this.marker.get('markerType'));
+      console.log('this is markers id:', this.marker.get('store_id'));
+      console.log('this is markers type:', this.marker.get('markerType'));
       console.log(this.marker.position);
       let content: string = 'Found Person';
-      this.addInfoWindow(this.marker, content);      
+      this.addInfoWindow(this.marker, content);
       //this.marker.setMap(this.map);
-     //this.markers.push(marker);
+      //this.markers.push(marker);
     }
   }
 
-  addMaarker(coords){
-    var marker =new google.maps.Marker({
-      position:coords,
-      map:this.map
+  addMaarker(coords) {
+    var marker = new google.maps.Marker({
+      position: coords,
+      map: this.map
     });
   }
 
 
 
-  updateSearchResults(){
+  updateSearchResults() {
     if (this.autocomplete.input == '') {
       this.autocompleteItems = [];
       return;
     }
     this.GoogleAutocomplete.getPlacePredictions({ input: this.autocomplete.input },
-    (predictions, status) => {
-      this.autocompleteItems = [];
-      this.zone.run(() => {
-        predictions.forEach((prediction) => {
-          this.autocompleteItems.push(prediction);
+      (predictions, status) => {
+        this.autocompleteItems = [];
+        this.zone.run(() => {
+          predictions.forEach((prediction) => {
+            this.autocompleteItems.push(prediction);
+          });
         });
       });
-    });
-  }  
-        // this.zone.run(function(){
-        //   predictions=predictions || [];
-        //   predictions.forEach(function(prediction){
-        //     console.log(prediction);
-        //     //this.autocompleteItems.push(prediction);
-        //     this.autocompleteItems.push(prediction);
-        //   });
-        // });
-        // this.zone.run(()=>{
-        //   predictions.forEach((prediction)=>{
-        //     this.autocompleteItems.push(prediction);
-        //   });
-        // });
-        //console.log("this.autocompleteItems);
-      
+  }
 
-    getLocation(){
 
-      this.geolocation.getCurrentPosition().then((resp) => {
-        this.latitude = resp.coords.latitude;
-        this.longitude = resp.coords.longitude;
-        this.map = new google.maps.Map(this.gmap.nativeElement, 
-          this.mapOptions1);
-        const infoWindow = new google.maps.InfoWindow;
-        const pos = {
-          lat: this.latitude,
-          lng: this.longitude
-        };
-        const icon = {
-          url: '../../assets/icon/YOU ARE HERE.png',
-          //url: 'https://img.icons8.com/ios-glyphs/24/000000/place-marker.png', // image url
-          scaledSize: new google.maps.Size(50, 50), // scaled size
-        };
-        
-        const marker = new google.maps.Marker({
-             position: pos,
-             map: this.map,
-             icon: icon
-          });
-        infoWindow.setPosition(pos);
-        infoWindow.setContent('Location found.');
-        infoWindow.open(this.map);
-        this.map.setCenter(pos);
-        this.geoMarker.setMap(this.map);
-      }).catch((error) => {
-        console.log('Error in getting the locations', error);
+  // mapOptions1: google.maps.MapOptions = {
+  //   center: this.coordinates,
+  //   disableDefaultUI: true,
+  //   zoom: 18
+  // };
+  getLocation() {
+
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.latitude = resp.coords.latitude;
+      this.longitude = resp.coords.longitude;
+      this.map = new google.maps.Map(this.gmap.nativeElement,
+        this.mapOptions1);
+      const infoWindow = new google.maps.InfoWindow;
+      const pos = {
+        lat: this.latitude,
+        lng: this.longitude
+      };
+      const icon = {
+        url: '../../assets/icon/YOU ARE HERE.png',
+        //url: 'https://img.icons8.com/ios-glyphs/24/000000/place-marker.png', // image url
+        scaledSize: new google.maps.Size(50, 50), // scaled size
+      };
+
+      const marker = new google.maps.Marker({
+        position: pos,
+        map: this.map,
+        icon: icon
       });
-      this.displayallmarkers();      
-    }
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      infoWindow.open(this.map);
+      this.map.setCenter(pos);
+      this.geoMarker.setMap(this.map);
+    }).catch((error) => {
+      console.log('Error in getting the locations', error);
+    });
+    this.displayallmarkers();
+  }
+  mapOptions2: google.maps.MapOptions = {
+    center: this.Searchposition,
+    disableDefaultUI: true,
+     zoom: 14
+   };
+  selectSearchResult(item) {
 
-    selectSearchResult(item){
-     // this.clearMarkers();
-      this.autocompleteItems = [];
     
-      this.geocoder.geocode({'placeId': item.place_id}, (results, status) => {
-        if(status === 'OK' && results[0]){
-          let position = {
-              lat: results[0].geometry.location.lat,
-              lng: results[0].geometry.location.lng
-          };
-          let marker = new google.maps.Marker({
-            position: results[0].geometry.location,
-            map: this.map,
-          });
-          this.markers.push(marker);
-          this.map.setCenter(results[0].geometry.location);
-        }
-      })
-    }
+       this.map = new google.maps.Map(this.gmap.nativeElement,
+        this.mapOptions2);
+    // this.clearMarkers();
+    this.autocompleteItems = [];
+
+    this.geocoder.geocode({ 'placeId': item.place_id }, (results, status) => {
+      console.log('hello adil bacha');
+      if (status === 'OK' && results[0]) {
+        this.Searchposition = {
+          lat: results[0].geometry.location.lat,
+          lng: results[0].geometry.location.lng
+        };
+        let marker = new google.maps.Marker({
+          position: results[0].geometry.location,
+          map: this.map,
+        });
+        this.markers.push(marker);
+        this.map.setCenter(results[0].geometry.location);
+      }
+    })
+  }
 
 
 }
